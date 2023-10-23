38d527764278d374e5db76ded2e57eceb2bb7a2b
shubhambokilwar <shubhambokilwar@gmail.com>
2023-10-23 19:27:18 +0530

    18h Oct file added

---
+++ 18_OCT_2023/Task1.py
@@ -0,0 +1,8 @@
+for counter in range(1, 10):
+    if counter == 5:
+        break
+    print(counter)
+print("For loop is finished")
+
+
+
---
+++ 18_OCT_2023/Task2.py
@@ -0,0 +1,7 @@
+
+count = 1
+while count <= 20:
+    print(count)
+    if count >= 10:
+        break
+    count = count + 1
